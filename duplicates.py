1635b1a7eb3ad7d7ab38966b73604c031a839932
cr00z <netrebinr@gmail.com>
2019-04-08 17:27:05 +0700

    make third attempt

--- duplicates.py
+++ duplicates.py
@@ -3,17 +3,19 @@ from collections import defaultdict
 # from colorama import Fore, Style  # Валл Ли сломался
 
 
-def find_duplicates(start_path):
-    dup_report = ''
+def get_file_candidates(start_path):
     dup_candidates = defaultdict(lambda: defaultdict(list))
     for directory, _, filenames in os.walk(start_path):
         for filename in filenames:
             filepath = os.path.join(directory, filename)
-            try:
+            if os.path.exists(filepath):
                 filesize = os.path.getsize(filepath)
                 dup_candidates[filename.lower()][filesize].append(filepath)
-            except FileNotFoundError:
-                pass
+    return dup_candidates
+
+
+def find_and_out_duplicates(dup_candidates):
+    dup_report = ''
     green_print_bgn = ''    # f'{Fore.GREEN}'
     green_print_end = ''    # f'{Style.RESET_ALL}'
     for filename, filesizes in dict(dup_candidates).items():
@@ -28,7 +30,8 @@ def find_duplicates(start_path):
 
 if __name__ == '__main__':
     start_path = input("Input start path to find duplicates: ")
-    dup_report = find_duplicates(start_path)
+    dup_candidates = get_file_candidates(start_path)
+    dup_report = find_and_out_duplicates(dup_candidates)
     if dup_report == '':
         print("No matches")
     else:
